7fabc340aed8dd11542d979d8108246d4df41171
Kiran <kirankumardn74@gmail.com>
2024-07-22 21:40:01 +0530

    files updated

--- config/db.js
+++ config/db.js
@@ -1,19 +1,17 @@
-const mongoose=require('mongoose')
+const mongoose=require('mongoose');
+const config=require('config');
+const db=config.get('mongoURI');
 
-const config=require('config')
-
-const db=config.get('mongoURI')
+console.log('entering to db file')
 
 const connectDB=async()=>{
-
     try {
+        console.log('entering to db')
         await mongoose.connect(db)
-        console.log("MongoDB Connected...")
-    } catch (err) {
-        console.error(err.message)
+        console.log('entering to after connection db')
+    } catch (error) {
         process.exit(1)
     }
-
 }
 
 module.exports=connectDB
