dcb803fd5f5f08c45497b0d396b2241fba907b58
Kiran <kirankumardn74@gmail.com>
2024-07-23 19:47:50 +0530

    token is updated in login files

--- src/index.js
+++ src/index.js
@@ -17,17 +17,25 @@ app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
 
-const bcrypt = require('bcryptjs')
+//const bcrypt = require('bcryptjs')
+const jwt=require('jsonwebtoken')
 
 const myFunction = async () => {
-    const password = 'Red12345!'
-    const hashedPassword = await bcrypt.hash(password, 8)
+    // const password = 'Red12345!'
+    // const hashedPassword = await bcrypt.hash(password, 8)
 
-    console.log(password)
-    console.log(hashedPassword)
+    // console.log(password)
+    // console.log(hashedPassword)
 
-    const isMatch = await bcrypt.compare('red12345!', hashedPassword)
-    console.log(isMatch)
+    // const isMatch = await bcrypt.compare('Red12345!', hashedPassword)
+    // console.log(isMatch)
+
+    const token=jwt.sign({_id:'hello123'},'thisismysignin',{expiresIn:'1 second'});
+    console.log(token)
+
+    const data=jwt.verify(token,'thisismysignin')
+    console.log(data)
+
 }
 
 myFunction()
